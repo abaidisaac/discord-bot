30cb94ee02617907a2645245fc2443aadfdea77a
Abaid Isaac <abaidisaacn@gmail.com>
2020-10-20 17:06:13 +0000

    first

---
+++ bot.py
@@ -0,0 +1,19 @@
+#!/home/dis/bin/python3
+import os
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = 'NzY2NTU2NjI3MTk2MTgyNTI5.X4lFfQ.b5kSDZt1oRnjCsbU7WvnB68fo1s'
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('hello'):
+        msg = await message.channel.send('gang gang yo')
+
+@client.event
+async def on_typing(channel,user,when):
+    print(user)
+
+client.run(TOKEN)
